afd30cdf45cdcf4bbb4509ba79b4537c00fe8346
lewandowskipiotr <93051544+lewandowskipiotr@users.noreply.github.com>
2021-11-02 21:43:16 +0100

    Add files via upload

---
+++ PPETask1_LinkedListLoopDetector/MyLinkedList.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PepeTask1_LinkedList
+{
+    class MyLinkedList
+    {
+        public Node Root;
+
+        public MyLinkedList(int value)
+        {
+            Root = new Node(value);
+        }
+
+        // Add node at the end of the list
+        public void AddNode(int value)
+        {
+            Node current = Root;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = new Node(value);
+        }
+
+        public Boolean ChecklooopFloyd()
+        {
+            Node slow = Root;
+            Node fast = Root;
+
+            while(slow != null && fast !=null && fast.Next !=null)
+            {
+                slow = slow.Next;
+                fast = fast.Next.Next;
+                if(slow == fast)
+                {
+                    Console.WriteLine("Loop detected");
+                    return true;
+                }
+
+            }
+            Console.WriteLine("No Loops detected");
+            return false;
+        }
+
+        public Boolean CheckloopFlags()
+        {
+            Node current = Root;
+            while (current != null)
+            {   
+                if(current.isVisited == true)
+                {
+                    Console.WriteLine("Loop detected");
+                    return true;
+                }
+                current.isVisited = true;
+                current = current.Next;            
+            }
+            Console.WriteLine("No loops detected");
+            return false;
+        }
+
+        public bool CheckLoopHash()
+        {
+            HashSet<Node> hashSet = new HashSet<Node>();
+            Node current = Root;
+            while (current != null)
+            {
+                if (hashSet.Contains(current))
+                {
+                    Console.WriteLine("Loop detected");
+                    return true;
+                }
+                hashSet.Add(current);
+                current = current.Next;
+            }
+            Console.WriteLine("No loops detected");
+            return false;
+        }
+
+
+            //public void ShowList(Node nextNode)
+            //{
+            //    Console.WriteLine(nextNode.Value);
+            //    if (nextNode.Next != null)
+            //    {
+            //        ShowList(nextNode.Next);
+            //    }
+            //}
+    }
+}
---
+++ PPETask1_LinkedListLoopDetector/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PepeTask1_LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyLinkedList list = new MyLinkedList(1);
+            list.AddNode(2);
+            list.AddNode(3);
+            list.AddNode(4);
+            //list.Root.Next.Next.Next = list.Root.Next.Next;
+
+            // list.ShowList(list.Root);
+
+            Console.WriteLine("Floyd method:");
+            list.ChecklooopFloyd();
+
+            Console.WriteLine("Flags method:");
+            list.CheckloopFlags();
+
+            Console.WriteLine("HashSet method:");
+            list.CheckLoopHash();
+
+            Console.ReadKey();
+        }
+    }
+}
