0356b80b0a7269957ec07ab8a3325be5070d5f35
lewandowskipiotr <93051544+lewandowskipiotr@users.noreply.github.com>
2021-11-02 21:27:35 +0100

    Add files via upload

---
+++ LinkedList/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PepeTask1_LinkedList
+{
+    class Node
+    {
+        public Node Next;
+        public int Value { get; set; }
+        public Boolean isVisited;
+        public  Node(int value)
+        {
+            Value = value;
+            Next = null;
+        }
+        public Node() { }
+
+    }
+}
