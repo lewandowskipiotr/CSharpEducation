391e3a88728252b3a20c740e237fce2d9bafd1b4
lewandowskipiotr <93051544+lewandowskipiotr@users.noreply.github.com>
2021-11-10 20:24:34 +0100

    Task2

---
+++ PPETask2_Palindrom/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PepeTask2_Palindrom
+{
+    class Program
+    {
+        public static Boolean isPalindrom(string text)
+        {
+            if(String.IsNullOrEmpty(text))
+            {
+                Console.WriteLine("Text is empty");
+                return false;
+            }
+            else
+            {
+                text = text.Replace(" ", "").ToLower();                
+                return text == new string(text.ToCharArray().Reverse().ToArray());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> Examples = new List<string>() { "Ala", "AdA", "Piotrek" , "Co mi dał duch cud ład i moc", "kajak","GłośNIK" };
+            foreach(var word in Examples)
+            {
+                if(isPalindrom(word))
+                {
+                    Console.WriteLine("'" + word + "' - correct");
+                }
+                else
+                {
+                    Console.WriteLine("'" + word + "' - incorrect");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
