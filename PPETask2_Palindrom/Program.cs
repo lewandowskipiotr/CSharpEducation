6a8e25c2a9a2aa64bd234bd32c38a08f4ece5a08
lewandowskipiotr <93051544+lewandowskipiotr@users.noreply.github.com>
2021-11-12 19:22:57 +0100

    optimalized

--- PPETask2_Palindrom/Program.cs
+++ PPETask2_Palindrom/Program.cs
@@ -6,26 +6,56 @@ namespace PepeTask2_Palindrom
 {
     class Program
     {
-        public static Boolean isPalindrom(string text)
+  
+        public static Boolean IsPalindromNaive(string text)
         {
             if(String.IsNullOrEmpty(text))
             {
-                Console.WriteLine("Text is empty");
+                Console.Write("Text is empty: ");
                 return false;
             }
             else
             {
-                text = text.Replace(" ", "").ToLower();                
+                text = String.Concat(text.Where(c => !Char.IsWhiteSpace(c))).ToLower();
                 return text == new string(text.ToCharArray().Reverse().ToArray());
+            }
+        }
+
+
+        public static Boolean IsPalindromOptimized(string text)
+        {
+            if (String.IsNullOrEmpty(text))
+            {
+                Console.Write("Text is empty: ");
+                return false;
+            }
+            else
+            {
+                text = String.Concat(text.Where(c => !Char.IsWhiteSpace(c))).ToLower();
+                int begin = 0;
+                int end = text.Length - 1;
+                while(begin < end)
+                {
+                    if(text[begin] == text[end])
+                    {
+                        begin++;
+                        end--;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                return true;                
             }
         }
 
         static void Main(string[] args)
         {
-            List<string> Examples = new List<string>() { "Ala", "AdA", "Piotrek" , "Co mi dał duch cud ład i moc", "kajak","GłośNIK" };
-            foreach(var word in Examples)
+            List<string> Examples = new List<string>() { "Ala", "AdA", "Piotrek", "Co mi dał duch cud ład i moc", "Kajak", "GłośNIK","" };
+            foreach (var word in Examples)
             {
-                if(isPalindrom(word))
+                if (IsPalindromOptimized(word))
                 {
                     Console.WriteLine("'" + word + "' - correct");
                 }
@@ -34,6 +64,7 @@ namespace PepeTask2_Palindrom
                     Console.WriteLine("'" + word + "' - incorrect");
                 }
             }
+
             Console.ReadKey();
         }
     }
