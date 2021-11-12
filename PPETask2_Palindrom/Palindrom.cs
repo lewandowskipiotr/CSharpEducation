bc866daf6c2f75be55a1fd13c73f1238150d6775
lewandowskipiotr <93051544+lewandowskipiotr@users.noreply.github.com>
2021-11-12 20:28:04 +0100

    separated palindrom

---
+++ PPETask2_Palindrom/Palindrom.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace PepeTask2_Palindrom
+{
+    public class Palindrom
+    {
+        public static Boolean IsPalindromNaive(string text)
+        {
+            if (String.IsNullOrEmpty(text))
+            {
+                Console.Write("Text is empty: ");
+                return false;
+            }
+            else
+            {
+                text = String.Concat(text.Where(c => !Char.IsWhiteSpace(c))).ToLower();
+                return text == new string(text.ToCharArray().Reverse().ToArray());
+            }
+        }
+
+
+        public static Boolean IsPalindromOptimized(string text)
+        {
+            if (String.IsNullOrEmpty(text))
+            {
+                Console.Write("Text is empty: ");
+                return false;
+            }
+            else
+            {
+                text = String.Concat(text.Where(c => !Char.IsWhiteSpace(c))).ToLower();
+                int begin = 0;
+                int end = text.Length - 1;
+                while (begin < end)
+                {
+                    if (text[begin] == text[end])
+                    {
+                        begin++;
+                        end--;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
