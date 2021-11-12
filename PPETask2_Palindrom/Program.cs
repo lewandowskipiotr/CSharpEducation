fd3393918f06de5fa5ff0f46e95545bce9e002ae
lewandowskipiotr <93051544+lewandowskipiotr@users.noreply.github.com>
2021-11-12 20:28:25 +0100

    separated palindrom

--- PPETask2_Palindrom/Program.cs
+++ PPETask2_Palindrom/Program.cs
@@ -6,56 +6,13 @@ namespace PepeTask2_Palindrom
 {
     class Program
     {
-  
-        public static Boolean IsPalindromNaive(string text)
-        {
-            if(String.IsNullOrEmpty(text))
-            {
-                Console.Write("Text is empty: ");
-                return false;
-            }
-            else
-            {
-                text = String.Concat(text.Where(c => !Char.IsWhiteSpace(c))).ToLower();
-                return text == new string(text.ToCharArray().Reverse().ToArray());
-            }
-        }
-
-
-        public static Boolean IsPalindromOptimized(string text)
-        {
-            if (String.IsNullOrEmpty(text))
-            {
-                Console.Write("Text is empty: ");
-                return false;
-            }
-            else
-            {
-                text = String.Concat(text.Where(c => !Char.IsWhiteSpace(c))).ToLower();
-                int begin = 0;
-                int end = text.Length - 1;
-                while(begin < end)
-                {
-                    if(text[begin] == text[end])
-                    {
-                        begin++;
-                        end--;
-                    }
-                    else
-                    {
-                        return false;
-                    }
-                }
-                return true;                
-            }
-        }
 
         static void Main(string[] args)
         {
             List<string> Examples = new List<string>() { "Ala", "AdA", "Piotrek", "Co mi dał duch cud ład i moc", "Kajak", "GłośNIK","" };
             foreach (var word in Examples)
             {
-                if (IsPalindromOptimized(word))
+                if (Palindrom.IsPalindromOptimized(word))
                 {
                     Console.WriteLine("'" + word + "' - correct");
                 }
